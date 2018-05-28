fc783a170869f39ebb1843a0f6a3b603f1b25870
qianxuebing12 <qianxuebing92@163.com>
2018-05-28 18:09:48 +0800

    first

---
+++ DailyPractice/BLOG/js/index.js
@@ -0,0 +1,21 @@
+$(function(){
+
+    // 返回顶部图标显示与隐藏
+    $(window).scroll(function(){
+        if ($(window).scrollTop()>100) {
+            $(".go-top").show();
+        } else {
+            $(".go-top").hide();
+        }
+    });
+    
+    // 返回顶部
+    $(".go-top").click(function(){
+        $("body,html").animate({scrollTop:0},300);
+    })
+
+
+
+
+
+})

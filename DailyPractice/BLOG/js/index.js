926df64fe805b84ced85f5cc10061f69706313eb
qianxuebing12 <qianxuebing92@163.com>
2018-05-29 11:27:26 +0800

    fourth

--- DailyPractice/BLOG/js/index.js
+++ DailyPractice/BLOG/js/index.js
@@ -17,11 +17,27 @@ $(function(){
     // Tab切换
     $(".header").on("click",".nav-list li",function(){
         $(this).addClass("active").siblings().removeClass("active");
-    })
+    });
 
-   
+    // 侧边菜单显示与隐藏
+    $(".btn-sidebar").click(function(){
+        showSideBar();
+    });
 
+    $(".mask").click(function () {
+        hideSideBar();
+    });
 
+    //显示
+    function showSideBar(){
+        $(".mask").fadeIn();
+        $(".side-bar").animate({right:"0"},300);
+    }
+    // 隐藏
+    function hideSideBar() {
+        $(".mask").fadeOut();
+        $(".side-bar").animate({right:"-260px"},300);
+    }
 
 
 })
